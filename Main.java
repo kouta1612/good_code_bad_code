f19a0fb441d761035f973f6f84bd6862a178b8c0
kouta1612 <kouta1612world69@gmail.com>
2022-06-26 17:09:37 +0900

    valueObject

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Currency;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Currency yen = Currency.getInstance(Locale.JAPAN);
+        Money money = new Money(1, yen);
+        Money otherMoney = new Money(100, yen);
+        Money addedMoney = money.add(otherMoney);
+        System.out.println(addedMoney.amount);
+    }
+}
---
+++ Money.java
@@ -0,0 +1,23 @@
+import java.util.Currency;
+
+public class Money {
+    final int amount;
+    final Currency currency;
+
+    Money(final int amount, final Currency currency) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("金額には0以上を指定してください。");
+        }
+        if (currency == null) {
+            throw new NullPointerException("通過単位を指定してください。");
+        }
+
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    Money add(final Money other) {
+        final int added = amount + other.amount;
+        return new Money(added, currency);
+    }
+}
